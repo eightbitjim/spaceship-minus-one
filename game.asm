fab1135c502892354cf09d69b7fc6573640d9a41
eightbitjim <eightbitjim@gmail.com>
2017-11-22 20:08:41 +0000

    Make loaders work

--- game.asm
+++ game.asm
@@ -16,7 +16,7 @@ restart
 				jsr scrollNow
 				jmp smoothScrollLoop
 				
-welcome			dc.b	147,18,31,">>>>>>>>>>>>>>>>000000",146,0
+welcome			dc.b	147,18,31, "                000000",146,0
 startMessage	dc.b	19,17,17,17,17,17,17,18, 5,29, 29, 29, "  SPACE SHIP 0 ", 13
 				dc.b	17,17,17,17, 159, 29, 29, 29, 18, "  SPACE / FIRE ",13,0
 				
@@ -1607,6 +1607,8 @@ explode subroutine
 				sty voice1
 				sty voice0
 				bne .loop
+				lda #3
+				sta borderPaper
 				rts
 
 startScreen     subroutine
